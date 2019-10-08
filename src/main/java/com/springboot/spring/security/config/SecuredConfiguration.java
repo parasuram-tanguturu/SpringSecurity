c1892f19ae88f84a6c01c41e189d4ff9aa9f997c
Parasuram Tanguturu <parasuram.tanguturu@siemens.com>
2019-10-08 16:51:05 +0530

    in memory authentication

--- src/main/java/com/springboot/spring/security/config/SecuredConfiguration.java
+++ src/main/java/com/springboot/spring/security/config/SecuredConfiguration.java
@@ -1,7 +1,25 @@
 package com.springboot.spring.security.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class SecuredConfiguration extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.inMemoryAuthentication().withUser("ram").password("ram").roles("USER");
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+		
+	}
 
 }
